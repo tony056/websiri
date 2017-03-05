342cb73f9d68990499c203404095d26615d431f6
Tony Tung <tony61507@gmail.com>
2017-03-04 21:55:53 -0800

    simple received message

--- index.js
+++ index.js
@@ -19,12 +19,37 @@ app.get('/', function(req, res){
 //for facebook verification
 app.get('/webhook/', function(req, res){
     if(req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me'){
-        res.send(req.query['hub.challenge'])
+        res.send(req.query['hub.challenge']);
     }
-    res.send('Error, wrong token')
-})
+    res.send('Error, wrong token');
+});
+
+//receive message
+app.post('/webhook', function(req, res){
+    var data = req.body;
+    if(data.object === 'page'){
+        data.entry.forEach(function(entry){
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
+
+            entry.messaging.forEach(function(event){
+                if(event.message){
+                    receivedMessage(event);
+                } else {
+                    console.log('unknown event');
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
+});
+
+function receivedMessage(event){
+    console.log('Message data: ' + event.message);
+}
 
 //sign up the server
 app.listen(app.get('port'), function(){
     console.log('running on port', app.get('port'))
-})
+});
