065ad73a97275714e80e169546e8d73879a33519
Tony Tung <tony61507@gmail.com>
2017-03-04 22:23:40 -0800

    added receive, send message sample

--- index.js
+++ index.js
@@ -41,14 +41,75 @@ app.post('/webhook', function(req, res){
             });
         });
 
-        res.sendStatus(200);
+        // res.sendStatus(200);
     }
 });
 
 function receivedMessage(event){
-    console.log('Message data: ' + event.message);
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if(messageText){
+        switch(messageText) {
+            case 'generic':
+                sendGenericMessage(senderID);
+                break;
+            default:
+                sendTextMessage(senderID, messageText);
+        }
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
+    }
 }
 
+function sendGenericMessage(recipientID, messageText){
+
+}
+
+function sendTextMessage(recipientId, messageText){
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: messageText
+        }
+    };
+    callSendAPI(messageData);
+}
+
+function callSendAPI(messageData){
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: 'EAATddErshOMBAKLaVAzPNVMjY4dVZAvNiGUTZAa2ZAflABUVh0yyEeER7motqD94UzTbDTxI47PnUes4ZAnXvb1PZACiHFV4DcI6FiOXsnh7e5gs4DpSCDZAm4qCTqwZCwwOsTZBJATlVhp5zuEv62ETyUjRjFZArAVwHLyxOBBZAQkAZDZD'},
+        method: 'POST',
+        json: messageData
+    }, function(error, response, body){
+        if(!error && response.statusCode === 200){
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            console.log("Successfully sent generic message with id %s to recipient %s",
+            messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
+    }
+
+    );
+}
 //sign up the server
 app.listen(app.get('port'), function(){
     console.log('running on port', app.get('port'))
