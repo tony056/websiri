838f8b5340676a82e14db322a89c08f8f9f74187
Tony Tung <tony61507@gmail.com>
2017-03-10 00:12:12 -0800

    followed current API

--- bot.js
+++ bot.js
@@ -41,28 +41,35 @@ var read = function (sender, message, reply) {
 		var sessionId = findOrCreateSession(sender)
 		// Let's forward the message to the Wit.ai bot engine
 		// This will run all actions until there are no more actions left to do
-		wit.runActions(
-			sessionId, // the user's current session by id
-			message,  // the user's message
-			sessions[sessionId].context, // the user's session state
-			function (error, context) { // callback
-			if (error) {
-				console.log('oops!', error)
-			} else {
-				// Wit.ai ran all the actions
-				// Now it needs more messages
-				console.log('Waiting for further messages')
-
-				// Based on the session state, you might want to reset the session
-				// Example:
-				// if (context['done']) {
-				// 	delete sessions[sessionId]
-				// }
-
-				// Updating the user's current session state
-				sessions[sessionId].context = context
-			}
-		})
+        wit.runActions(sessionId, message, sessions[sessionId].context)
+            .then(function(context) {
+                console.log('Waiting for next user messages');
+                sessions[sessionId].context = context;
+            }).catch(function(err)  {
+              console.error('Oops! Got an error from Wit: ', err.stack || err);
+          });
+		// wit.runActions(
+		// 	sessionId, // the user's current session by id
+		// 	message,  // the user's message
+		// 	sessions[sessionId].context, // the user's session state
+		// 	function (error, context) { // callback
+		// 	if (error) {
+		// 		console.log('oops!', error)
+		// 	} else {
+		// 		// Wit.ai ran all the actions
+		// 		// Now it needs more messages
+		// 		console.log('Waiting for further messages')
+        //
+		// 		// Based on the session state, you might want to reset the session
+		// 		// Example:
+		// 		// if (context['done']) {
+		// 		// 	delete sessions[sessionId]
+		// 		// }
+        //
+		// 		// Updating the user's current session state
+		// 		sessions[sessionId].context = context
+		// 	}
+		// })
 	}
 }
 
